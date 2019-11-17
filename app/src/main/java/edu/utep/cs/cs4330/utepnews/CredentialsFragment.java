1426d33782b0c76fdbbcf0d97bce314a343be33e
Tomas Patro <tomas.patro@gmail.com>
2019-11-16 21:59:26 -0700

    Added logic for registrations

--- app/src/main/java/edu/utep/cs/cs4330/utepnews/CredentialsFragment.java
+++ app/src/main/java/edu/utep/cs/cs4330/utepnews/CredentialsFragment.java
@@ -1,10 +1,14 @@
 package edu.utep.cs.cs4330.utepnews;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CredentialsFragment extends Fragment {
     private static final int LOGIN_LAYOUT = 0;
@@ -25,6 +35,8 @@ public class CredentialsFragment extends Fragment {
     private TextView aboutTextView;
     private TextView contactTextView;
     private TextView actionTextView;
+
+    private FirebaseAuth auth;
 
     public CredentialsFragment() {}
 
@@ -41,6 +53,8 @@ public class CredentialsFragment extends Fragment {
         aboutTextView = view.findViewById(R.id.aboutTextView);
         contactTextView = view.findViewById(R.id.contactTextView);
         actionTextView = view.findViewById(R.id.actionTextView);
+
+        auth = FirebaseAuth.getInstance();
 
         return inflater.inflate(R.layout.fragment_credentials, container, false);
     }
@@ -61,7 +75,48 @@ public class CredentialsFragment extends Fragment {
         }
     }
 
-    private void registerClicked(View view) {}
+    private void registerClicked(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordAgain = passwordAgainEditText.getText().toString();
+
+        if (!isValidEmail(email)) {
+            Toast.makeText(view.getContext(), "Invalid email!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(view.getContext(), "Password can not be empty!",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!TextUtils.equals(password, passwordAgain)) {
+            Toast.makeText(view.getContext(), "Passwords do not match!",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener((@NonNull Task<AuthResult> task) -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(view.getContext(), "Registration successful!",
+                                Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
+
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(view.getContext(), "Registration failed!",
+                                Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
 
     private void loginClicked(View view) {}
+
+    private boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
 }
